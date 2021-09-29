0cf5e600cb7d8876aec82a243bca43b9117832e9
Felix Huber <felix.huber@ametras.com>
2021-09-29 12:00:23 +0200

    Initial commit

---
+++ app/src/main/java/nyx/snippet/compose/MainActivity.kt
@@ -0,0 +1,47 @@
+package nyx.snippet.compose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import nyx.snippet.compose.ui.theme.Pa1snippetcomposeTheme
+import nyx.snippet.compose.ui.theme.Teal200
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Pa1snippetcomposeTheme {
+                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                    MyHelloWorld(null)
+
+                    MyGreeting()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MyHelloWorld(someText: String?) = Text(text = someText ?: "someText is null.")
+
+@Composable
+fun MyGreeting() {
+    val names = listOf("Amir", "Bob", "Carol", "Demeter", "Erebos", "Felix")
+    var greeting by remember { mutableStateOf("") }
+
+    Column(Modifier.background(Teal200)) {
+        Text(greeting)
+        Button(onClick = {
+            greeting = "Hello ${names.random()}!"
+        }) {
+            Text("say hello")
+        }
+    }
+}
